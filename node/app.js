328edcd710421fc0797ddd452b259a593ade1e42
NikolaKecan <72013072+NikolaKecan@users.noreply.github.com>
2021-02-14 14:24:23 +0100

    vue1

--- node/app.js
+++ node/app.js
@@ -1,13 +1,27 @@
 const express = require('express');
-
 const jks = require('./routes/jokes.js');
+const history = require('connect-history-api-fallback');
+const path = require('path');
+
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET", "PUT", "POST", "DELETE", "OPTIONS")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+
 app.use('/api', jks);
 
-app.get('/', ((req, res) => {
-    res.send('Zdravo!');
-}));
+const staticDir = express.static(path.join(__dirname, 'dist'))
+
+//ukljucimo middleware-ove
+app.use(staticDir);
+app.use(history);
+app.use(staticDir);
 
 app.listen(80);
